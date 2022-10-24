27419d1d706fae2f28354098a816648d79c3be80
Asmit2952 <asmitbm2952002@gmail.com>
2022-10-24 19:56:40 +0530

    minor changes

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,11 +1,11 @@
 export default function Meme () {
     return (
         <div className="meme">
-            <form className="form">
+            <div className="form">
             <input type="text" id="fname" name="fname" placeholder="Top Text" className="form-input"/>
             <input type="text" id="lname" name="lname" placeholder="Bottom Text" className="form-input"/>
             <button className="form-button">Get a new meme image 🖼️</button>
-            </form>
+            </div>
         </div>
     )
 }
